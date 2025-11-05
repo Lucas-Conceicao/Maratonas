49bb95af2b3eb4de122a2d3c0a9d60da66e035e8
Lucas Conceição Ricardo <161897897+Lucas-Conceicao@users.noreply.github.com>
2025-11-04 21:14:26 -0300

    exercício descendes realizado

---
+++ Internas/2025/descendentes.java
@@ -0,0 +1,33 @@
+//PROBLEMA D DESCENDENTES
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class descendentes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int linhas = scanner.nextInt(), x;
+        List<Integer> genitor = new ArrayList<>(), descendente = new ArrayList<>();
+
+        for(x = 0; x < linhas; x++){
+            genitor.add(scanner.nextInt());
+            descendente.add(scanner.nextInt());
+        }
+        int escolhido = scanner.nextInt();
+        System.out.println(verificarDescendentes(escolhido, genitor, descendente));
+
+    }
+
+    public static int verificarDescendentes(int pessoa, List<Integer> genitor, List<Integer> descendente){
+        int count = 0, x;
+
+        for(x = 0; x < genitor.size();x++){
+            if(genitor.get(x) == pessoa){
+                int filho = descendente.get(x);
+                count++;
+                count = count + verificarDescendentes(filho, genitor, descendente); // chama recursivamente o filho direto, depois os filhos dele e assim sucesivamente. Voltando para o for anterior ao chegar finalizar o loop;
+            }
+        }
+        return count;
+    }
+}
